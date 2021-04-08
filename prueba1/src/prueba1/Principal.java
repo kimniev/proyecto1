1f4f3d52c8f679ee288da0da9896d525da7a30a6
Alvaro <kimniev@hotmail.com>
2021-04-08 16:43:17 +0200

    Segundo cambio. Momento 2

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,8 +3,10 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hola mundo java-git");
+		
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+	
 	}
 
 }
